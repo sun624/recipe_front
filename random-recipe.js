4c6e1ca522b38afddd8aaa2e683dfef940c90d6a
Ethan <sunyichun624@gmail.com>
2021-04-28 00:13:16 -0700

    use api placeholder due to daily limit.

--- random-recipe.js
+++ random-recipe.js
@@ -1,25 +1,26 @@
-function fetchData() {
+async function fetchData() {
   //fetch("https://reqres.in/api/users")
-  fetch("https://recipe-finder-group404.herokuapp.com/index.html")
-    .then((response) => {
-      if (!response.ok) {
-        throw Error("Error");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      createItemCard(data);
-      console.log(data);
+  const res = await fetch("https://reqres.in/api/users");
+  const data = await res.json();
+  return data.data[0]
       // console.log(data.image);
       // const html = data.data.map((user) => {
       //   return `<div class="user"><p>Title: ${user.title}</p> <img src="${user.image}" /> </div>`;
       // });
       //console.log(html);
-    });
+  
 }
-fetchData();
 
-function createItemCard(data) {
+randomRecipes();
+
+async function randomRecipes(){
+  for (let i = 1; i < 5; i++) {
+    createItemCard(await fetchData(), i);
+  }
+}
+
+
+function createItemCard(data,i) {
   let chooseItems = document.createElement("div");
   // chooseItems.setAttribute("class", "card");
   // chooseItems.style.width = "15rem";
@@ -42,8 +43,8 @@ function createItemCard(data) {
   // cardBody.appendChild(cardTitle);
   // chooseItems.appendChild(cardBody);
 
-  chooseItems.innerHTML = `<div class="user"><p>Title: ${data.title}</p> <img src="${data.image}" /> </div>`;
-  document.querySelector("#items_container").appendChild(chooseItems);
+  chooseItems.innerHTML = `<div class="user"><h4>${data.email}</h4> <img width="300px" src="${data.avatar}"/> </div>`;
+  document.querySelector(`#items_container${i}`).appendChild(chooseItems);
   // .insertAdjacentElement("afterbegin", html);
   //return chooseItems;
 }
