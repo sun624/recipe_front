b4e2947d81e3c638fba3a00c328f67ac9cc282bb
shpasang <pasang.sherpa2044@gmail.com>
2021-04-28 12:26:02 -0700

    updated the modal form

--- createRecipe/recipeapp.js
+++ createRecipe/recipeapp.js
@@ -23,14 +23,3 @@ const navSlide = () => {
 };
 
 navSlide();
-
-const open = document.getElementById("open");
-const modal_container = document.getElementById("modal_container");
-const close = document.getElementById("submit");
-
-open.addEventListener("click", () => {
-  modal_container.classList.add("show");
-});
-close.addEventListener("click", () => {
-  modal_container.classList.remove("show");
-});
