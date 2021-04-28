2ba5bcf5f6d8dbdcf88d9c56ac46a6be4b7268d1
Rasoj Timsina <rasoj_timsina@yahoo.com>
2021-04-28 15:05:50 -0500

    footercss added

--- random-recipe.js
+++ random-recipe.js
@@ -1,26 +1,27 @@
 async function fetchData() {
   //fetch("https://reqres.in/api/users")
+  // const res = await fetch(
+  //   "https://recipe-finder-group404.herokuapp.com/index.html"
+  // );
   const res = await fetch("https://reqres.in/api/users");
   const data = await res.json();
-  return data.data[0]
-      // console.log(data.image);
-      // const html = data.data.map((user) => {
-      //   return `<div class="user"><p>Title: ${user.title}</p> <img src="${user.image}" /> </div>`;
-      // });
-      //console.log(html);
-  
+  return data.data[0];
+  // console.log(data.image);
+  // const html = data.data.map((user) => {
+  //   return `<div class="user"><p>Title: ${user.title}</p> <img src="${user.image}" /> </div>`;
+  // });
+  //console.log(html);
 }
 
 randomRecipes();
 
-async function randomRecipes(){
+async function randomRecipes() {
   for (let i = 1; i < 5; i++) {
     createItemCard(await fetchData(), i);
   }
 }
 
-
-function createItemCard(data,i) {
+function createItemCard(data, i) {
   let chooseItems = document.createElement("div");
   // chooseItems.setAttribute("class", "card");
   // chooseItems.style.width = "15rem";
